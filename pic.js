35da1b6ff3a66b9d386b68b42dc6ebc2687db221
swust-xiaoj <swustxiaojie@163.com>
2015-05-10 14:51:18 +0800

    完成了上下左右四个方面的拖动，四个角落的拖动还未完成

--- pic.js
+++ pic.js
@@ -1,25 +1,67 @@
 /*2015年5月10日13:39:39*/
 window.onload = function(){
 	var rightDiv = document.getElementById("right");
+	var topDiv = document.getElementById("top");
+	var leftDiv = document.getElementById("left");
+	var bottomDiv = document.getElementById("bottom");
 	var mainDiv = document.getElementById("main");
 	var ifKeyDown = false;
+	var contact = "";//按下的触点
 
 	rightDiv.onmousedown = function(){
 		ifKeyDown = true;
+		contact = "right"
 	}
 
-	rightDiv.onmouseup = function(){
+	topDiv.onmousedown = function(){
+		ifKeyDown = true;
+		contact = "top";
+	}
+
+	leftDiv.onmousedown = function(){
+		ifKeyDown = true;
+		contact = "left";
+	}
+
+	bottomDiv.onmousedown = function() {
+		ifKeyDown = true;
+		contact = "bottom";
+	}
+
+	window.onmouseup = function(){
 		ifKeyDown = false;
 	}
 
 	window.onmousemove = function(e) {
 		if(ifKeyDown == true){
-			alert(mainDiv.offsetWidth);
-			var x = e.clientX;//鼠标x位置
-			var addWidth = "";//增加的宽度
-			var widthBefore = mainDiv.offsetWidth - 2;
-			addWidth = x - getElemPosition(mainDiv).left - widthBefore;
-			mainDiv.style.width = addWidth + widthBefore + "px";
+			if(contact == "right"){
+				var x = e.clientX;//鼠标x位置
+				var addWidth = "";//增加的宽度
+				var widthBefore = mainDiv.offsetWidth - 2;
+				addWidth = x - getElemPosition(mainDiv).left - widthBefore;
+				mainDiv.style.width = addWidth + widthBefore + "px";
+			}
+			else if(contact == "top"){
+				var y = e.clientY;
+				var mainY = getElemPosition(mainDiv).top;
+				var addHeight = mainY - y;
+				var heightBefore = mainDiv.offsetHeight - 2 ;
+				mainDiv.style.height = heightBefore + addHeight + "px";
+				mainDiv.style.top = mainDiv.offsetTop - addHeight + "px";
+			}
+			else if(contact == "left"){
+				var x = e.clientX;
+				var widthBefore = mainDiv.offsetWidth - 2;
+				var addWidth = getElemPosition(mainDiv).left - x;
+				mainDiv.style.width = addWidth + widthBefore + "px";
+				mainDiv.style.left = mainDiv.offsetLeft - addWidth + "px";
+			}
+			else if(contact == "bottom"){
+				var y = e.clientY;
+				var heightBefore = mainDiv.offsetHeight - 2;
+				var addHeight = y - getElemPosition(mainDiv).top - heightBefore;
+				mainDiv.style.height = heightBefore + addHeight + "px";
+			}
 		}
 	}
 }
